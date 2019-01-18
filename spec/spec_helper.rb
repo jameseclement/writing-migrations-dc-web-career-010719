1478afab3a4cc207ccc5d6c2cce93b7e66ae9184
James Clement <jameseclement@gmail.com>
2019-01-18 15:26:28 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-ENV["SCHOOL_ENV"] = "test"
+# ENV["SCHOOL_ENV"] = "test"
 
-require_relative '../config/environment'
+# require_relative '../config/environment'
 
-RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.order = 'default'
-end
+# RSpec.configure do |config|
+#   config.run_all_when_everything_filtered = true
+#   config.order = 'default'
+# end
